922428d85513d0eca501ca85e92b81ff03b0b453
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-01-30 18:02:24 +0100

    implemented /setspawn and /spawn system, v1.4.3

--- src/main/kotlin/de/florianmichael/testserverutils/command/impl/WrappedCommandSpawn.kt
+++ src/main/kotlin/de/florianmichael/testserverutils/command/impl/WrappedCommandSpawn.kt
@@ -1,35 +1,24 @@
 package de.florianmichael.testserverutils.command.impl
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
-import de.florianmichael.testserverutils.TestServerUtils
 import de.florianmichael.testserverutils.command.WrappedCommand
 import de.florianmichael.testserverutils.command.brigadier.SpigotCommandSource
 import de.florianmichael.testserverutils.config.ConfigurationWrapper
 import de.florianmichael.testserverutils.extension.craftbukkit.checkPermission
 import de.florianmichael.testserverutils.extension.craftbukkit.checkPlayer
 import de.florianmichael.testserverutils.extension.craftbukkit.prefixedMessage
-import org.bukkit.Location
 import org.bukkit.entity.Player
 
-class WrappedCommandSetSpawn : WrappedCommand("setspawn") {
+class WrappedCommandSpawn : WrappedCommand("spawn") {
 
-    private val permission = ConfigurationWrapper.unwrapString("commands.setspawn.permission")
-    private val message = ConfigurationWrapper.unwrapString("commands.setspawn.message")
-
-    companion object {
-        lateinit var spawnPoint: Location
-    }
-
-    init {
-        spawnPoint = TestServerUtils.get().config.get("commands.setspawn.spawn-position") as Location
-    }
+    private val permission = ConfigurationWrapper.unwrapString("commands.spawn.permission")
+    private val message = ConfigurationWrapper.unwrapString("commands.spawn.message")
 
     override fun builder(builder: LiteralArgumentBuilder<SpigotCommandSource>): LiteralArgumentBuilder<SpigotCommandSource> {
         return builder.executes {
             it.source.sender.apply {
                 if (checkPlayer() && checkPermission(permission)) {
-                    TestServerUtils.get().config.set("commands.setspawn.spawn-position", (this as Player).location)
-                    spawnPoint = this.location
+                    (this as Player).teleport(WrappedCommandSetSpawn.spawnPoint)
                     prefixedMessage(message)
                 }
             }
